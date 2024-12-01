092a593dbf54e06bbb9d88a09717f7df55cc7c62
Carlos Fernandes <69088161+carlosfernandescrypt@users.noreply.github.com>
2024-12-01 00:55:51 -0300

    Update DBConnection.java

--- floricultura/src/main/java/org/example/DBConnection.java
+++ floricultura/src/main/java/org/example/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/floricultura";
     private static final String USER = "root";
-    private static final String PASSWORD = "carlos41"; // Substitua pela sua senha
+    private static final String PASSWORD = "root"; // Substitua pela sua senha
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
